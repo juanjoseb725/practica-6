ede8f8b0d7dc5d4c0ae9e04f6e5c7f7d5dd6d0e7
juanjoseb725 <juanjoseb725@gmail.com>
2026-03-19 14:10:22 -0600

    Add comments to ejercicio1.c
    Add comments to explain code functionality and usage.

---
+++ ejercicio1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    // Comentario por línea
+
+    /* Comentario por bloque
+       que puede ocupar varios renglones */
+
+    // Este código compila y ejecuta, pero no muestra salida alguna
+    // debido a que los comentarios no se toman en cuenta.
+
+    /* Ya sea por línea o por bloque,
+       los comentarios no son tomados en cuenta al momento
+       de compilar el programa.
+       Sólo sirven como documentación en el código fuente. */
+
+    return 0;
+}
